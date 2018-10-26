a809ed8302955b089d814b3be8694fa155db0df5
asnowfox <tonnylee@eyou.com>
2018-10-27 03:53:26 +0800

    fix print

--- netflow/v9/decoder.go
+++ netflow/v9/decoder.go
@@ -451,7 +451,7 @@ func (d *Decoder) decodeSet(mem MemCache, msg *Message) error {
 				mem.insert(tr.TemplateID, d.raddr, tr)
 			}
 			msg.TemplaRecord = tr
-			fmt.Printf(" decoder receive a template record id is %d ",setHeader.FlowSetID)
+			fmt.Printf(" decoder receive a template record id is %d \n",tr.TemplateID)
 		} else if setId >= 4 && setId <= 255 {
 			// Reserved set, do not read any records
 			break
