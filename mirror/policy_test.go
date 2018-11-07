9dd6fd51b3826ee9e2244b29e9fb4411fdc37907
asnowfox <tonnylee@eyou.com>
2018-11-07 16:33:33 +0800

    add test output

--- mirror/policy_test.go
+++ mirror/policy_test.go
@@ -8,10 +8,9 @@ import (
 func TestLoadPolicy(t *testing.T) {
 	LoadPolicy("../scripts/policy.conf")
 	for _,policy := range policyConfigs {
-		fmt.Printf("policy:",policy.PolicyId)
+		fmt.Printf("policy: %s",policy.PolicyId)
 		for _,p := range policy.Policies {
 			fmt.Printf("  source %s, inport %d,outport %d, dst %s",p.Source,p.InPort,p.OutPort,p.DistAddress)
 		}
-
 	}
 }
