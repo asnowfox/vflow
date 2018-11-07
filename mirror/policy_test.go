233d6644e35701020ff0dd8a787df04939eba9b3
asnowfox <tonnylee@eyou.com>
2018-11-07 16:49:47 +0800

    policy read

--- mirror/policy_test.go
+++ mirror/policy_test.go
@@ -9,7 +9,7 @@ func TestLoadPolicy(t *testing.T) {
 	LoadPolicy("../scripts/policy.conf")
 	for _,policy := range policyConfigs {
 		fmt.Printf("policy: %s\n",policy.PolicyId)
-		for _,p := range policy.Policies {
+		for _,p := range policy.Rules {
 			fmt.Printf("  source %s, inport %d,outport %d, dst %s\n",
 				p.Source,p.InPort,p.OutPort,p.DistAddress)
 		}
