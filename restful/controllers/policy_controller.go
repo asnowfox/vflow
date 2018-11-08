903def5658879bed2a8ae0875ccc8373322e7710
asnowfox <tonnylee@eyou.com>
2018-11-08 10:45:35 +0800

    add delete support

--- restful/controllers/policy_controller.go
+++ restful/controllers/policy_controller.go
@@ -47,12 +47,19 @@ func (o *PolicyController) Delete() {
 	policyId := o.GetString("policyId")
 	fmt.Printf("call delete method of mirror controller, sourceId is %s\r\n", policyId)
 
-	index := -1
 	if policyId != "" {
-		index = mirror.DeletePolicy(policyId)
+		index,msg := mirror.DeletePolicy(policyId)
+		json := map[string]interface{}{}
+		json["result"] = index
+		json["message"] =  msg
+		o.Data["json"] = json
+		o.ServeJSON()
+		return
 	}
+
 	json := map[string]interface{}{}
-	json["result"] = index
+	json["result"] = -1
+	json["message"] = "no policy Id defined"
 	o.Data["json"] = json
 	o.ServeJSON()
 	return
