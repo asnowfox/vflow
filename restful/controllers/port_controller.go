ae536475ee96689f27af11c24ca80e1989b8c05d
litong <litong@iie.ac.cn>
2019-11-10 10:42:58 +0800

    fix index length is bigger than ifDes length in some huawei devices.

--- restful/controllers/port_controller.go
+++ restful/controllers/port_controller.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/VerizonDigital/vflow/snmp"
 	"github.com/astaxie/beego"
+	"sort"
 )
 
 // Operations about object
@@ -11,13 +12,14 @@ type PortController struct {
 	beego.Controller
 }
 
-
-
 func (o *PortController) Get() {
 	deviceIp := o.GetString("deviceIp")
 	fmt.Printf("Call get method of device controller")
-	devConfigs := snmp.ManageInstance.ListPortInfo(deviceIp)
-	o.Data["json"] = devConfigs
+	ports := snmp.ManageInstance.ListPortInfo(deviceIp)
+	sort.Slice(ports, func(i, j int) bool {
+		return ports[i].IfIndex < ports[j].IfIndex
+	})
+	o.Data["json"] = ports
 	o.ServeJSON()
 	return
 }
